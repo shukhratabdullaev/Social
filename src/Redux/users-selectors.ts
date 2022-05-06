a98c50a2c1f5afb173e6898916d5fa1cce3bda78
shukhratabdullaev <Pashaa2005@gmail.com>
2022-05-06 17:30:06 +0400

    81

--- src/Redux/users-selectors.ts
+++ src/Redux/users-selectors.ts
@@ -1,8 +1,13 @@
+import { createSelector } from "reselect";
 import { AppStateType } from "./redux-store";
 
-export const getUsers = (state: AppStateType) => {
+const getUsersSelector = (state: AppStateType) => {
     return state.usersPage.users
 }
+
+export const getUsers = createSelector(getUsersSelector, (users) => {
+    return users.filter(u => true)
+})
 
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize
